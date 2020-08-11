b89dc2c46ab7e73e8a86b224618d937ee6f95b8f
nakadorx <nakadorx@gmail.com>
2020-08-11 08:16:39 +0200

    user.py

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This modules defines aa class User"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
