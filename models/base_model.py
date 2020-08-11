6d6b103aebd6b4a921cca71175ee57107cf2b9de
nakadorx <nakadorx@gmail.com>
2020-08-11 08:21:59 +0200

    fix user.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,10 @@ from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
-Base = declarative_base()
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
