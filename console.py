6465d408e0f3f02d8431f54b989b2974f7d4e190
nakadorx <nakadorx@gmail.com>
2020-08-13 20:06:29 +0200

    commit

--- console.py
+++ console.py
@@ -15,7 +15,7 @@ from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Contains the functionality for the HBNB consol"""
+    """ Contains the functionality for the HBNB console"""
 
     # determines prompt for interactive/non-interactive modes
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
