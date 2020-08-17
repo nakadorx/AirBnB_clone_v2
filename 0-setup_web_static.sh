dda28bb98ba8bf1d74a62a4ff3a0b480879d6538
nakadorx <nakadorx@gmail.com>
2020-08-17 18:58:48 +0100

    rf

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,39 +1,10 @@
 #!/usr/bin/env bash
-#Bash script that sets up your web servers for the deployment of web_static
-
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
-
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "keep quiet plz" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-
-printf %s "server {
-	listen 80;
-	listen [::]:80 default_server;
-	root   /usr/share/nginx/html;
-	index  index.html index.htm;
-    add_header X-Served-By $HOSTNAME;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-		return 301 http://youtube.com/;
-	}
-
-	error_page 404 /notfound.html;
-	location = /notfound {
-		root /usr/share/nginx/html;
-		internal;
-	}
-}" > /etc/nginx/sites-available/default
-
+sudo sed -i '31i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
