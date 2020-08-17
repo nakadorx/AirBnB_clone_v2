3eb512c8c7939c7a9bde5c4e2545578322d735d1
IhebChatti <iheb.chatti@holbertonschool.com>
2020-08-17 15:22:38 +0100

    fixed script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -39,14 +39,14 @@ def do_deploy(archive_path):
     path1 = "/data/web_static/releases/{}/web_static/*".format(filename)
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
             format(filename, filename))
-        run("rm /tmp/{}".format(filename))
-        run("mv {} /data/web_static/releases/{}/".format(path1, filename))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+        run("sudo rm /tmp/{}".format(filename))
+        run("sudo mv {} /data/web_static/releases/{}/".format(path1, filename))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
             format(filename))
         return True
     except:
