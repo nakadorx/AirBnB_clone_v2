6c7346f2c8674a434f894098129022cdd30b0734
nakadorx <nakadorx@gmail.com>
2020-08-17 20:03:14 +0100

    gre

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""[Fabric script that generates a .tgz archive from the contents
-    of the web_static folder of your AirBnB Clone repo]
+"""
+cmnt
 """
 from fabric.api import local
 from datetime import datetime
@@ -8,11 +8,12 @@ from os import path
 
 
 def do_pack():
-    """[generates a .tgz archive]
+    """cmnt
     """
-    time_format = '%Y%m%d%H%M%S'
-    time = datetime.utcnow().strftime(time_format)
+    timeF = '%Y%m%d%H%M%S'
+    time = datetime.utcnow().strftime(timeF)
     filepath = "versions/web_static_{}.tgz".format(time)
+
     local("mkdir -p versions")
     local("tar -cvzf {} web_static".format(filepath))
     if path.exists(filepath):
