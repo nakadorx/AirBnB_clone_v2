fe8bafeddcc0ac785da7352120da3fa77de4838d
Montassar Daimi <52794132+nakadorx@users.noreply.github.com>
2020-08-17 19:57:56 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,54 +1,28 @@
 #!/usr/bin/python3
-"""[Fabric script]
 """
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
-from fabric.api import local
-from datetime import datetime
-from os import path
-
-env.hosts = ['34.75.254.10', '34.75.125.125']
-env.user = "ubuntu"
-
-
-def do_pack():
-    """[generates a .tgz archive]
-    """
-    time_format = '%Y%m%d%H%M%S'
-    time = datetime.utcnow().strftime(time_format)
-    filepath = "versions/web_static_{}.tgz".format(time)
-    local("mkdir -p versions")
-    local("tar -cvzf {} web_static".format(filepath))
-    if path.exists(filepath):
-        return filepath
-    else:
-        return None
+comnt
+"""
+from os.path import exists
+from fabric.api import put, run, env
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
-    """[distributes an archive to your web servers]
-
-    Args:
-        archive_path ([path]): [path of the archive file]
-    """
-    if not path.exists(archive_path) and path.isfile(archive_path):
+    """cont """
+    if exists(archive_path) is False:
         return False
-    filename = archive_path.split("/")[-1].split(".")[0]
-    path1 = "/data/web_static/releases/{}/web_static/*".format(filename)
     try:
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
-            format(filename, filename))
-        run("sudo rm /tmp/{}.tgz".format(filename))
-        run("sudo mv {} /data/web_static/releases/{}/".format(path1, filename))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".
-            format(filename))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(filename))
+        fileName = archive_path.split("/")[-1]
+        noEx = fileName.split(".")[0]
+        pt = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(pt, noEx))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(fileName, pt, noEx))
+        run('rm /tmp/{}'.format(fileName))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(pt, noEx))
+        run('rm -rf {}{}/web_static'.format(pt, noEx))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(pt, noEx))
         return True
     except:
-        return False
-    return True
+        return False*
