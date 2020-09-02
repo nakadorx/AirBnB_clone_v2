e480d03dcb10b2bdb6b653db7b03885c50173c75
nakadorx <nakadorx@gmail.com>
2020-09-02 16:24:37 +0200

    reg

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,8 +18,9 @@ def teardown(error):
 def statesList():
     """statesList
     """
-    st = storage.all("State").values()
+    st = storage.all("State")
     return render_template('7-states_list.html', states=st)
+
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 5000)
