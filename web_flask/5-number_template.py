044f3b9dd0b907bfbf90364e45bcf4959262b4c5
nakadorx <nakadorx@gmail.com>
2020-09-02 15:27:08 +0200

    01

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""script
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHBNB():
+    """despla hello hbnb
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """display hbnb
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ccc
+    """
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def pythonIsCool(text):
+    """pyhton is cool
+    """
+    return "Python %s" % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """display n
+    """
+    return "%d is a number" % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numberTemplate(n):
+    """display html
+    """
+    return render_template("5-number.html", num=n)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
